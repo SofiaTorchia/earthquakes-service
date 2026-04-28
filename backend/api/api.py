b2057c2e36eb74eb75f78d138c795af037831322
SofiaTorchia <sofhia388@gmail.com>
2026-04-28 17:03:56 +0200

    moving connection to railway db

--- backend/api/api.py
+++ backend/api/api.py
@@ -8,22 +8,21 @@ import os
 from fastapi import FastAPI
 import psycopg
 
-conn_general = psycopg.connect(
-    dbname=os.getenv("POSTGRES_DB", "postgresDB"),
-    user=os.getenv("POSTGRES_USER", "postgres"),
-    password=os.getenv("POSTGRES_PASSWORD", "pa"),
-    host=os.getenv("POSTGRES_HOST", "localhost"),
-    port=os.getenv("POSTGRES_PORT", "5432"),
-)
 
-conn_railway = psycopg.connect(
-    dbname=os.getenv("PGDATABASE"),
-    user=os.getenv("PGUSER"),
-    password=os.getenv("PGPASSWORD"),
-    host=os.getenv("PGHOST"),
-    port=os.getenv("PGPORT"),
-)
-conn = conn_railway
+def start_railway_db_connection() -> psycopg.Connection:
+    """
+    Starts connection to Postgres database
+    """
+    conn = psycopg.connect(
+        dbname=os.getenv("PGDATABASE"),
+        user=os.getenv("PGUSER"),
+        password=os.getenv("PGPASSWORD"),
+        host=os.getenv("PGHOST"),
+        port=os.getenv("PGPORT"),
+    )
+
+    logging.info("Started connection to Postgres Database")
+    return conn
 
 
 logging.basicConfig(
@@ -31,6 +30,7 @@ logging.basicConfig(
 )
 
 app = FastAPI()
+start_railway_db_connection()
 
 
 @app.get("/earthquakes")
