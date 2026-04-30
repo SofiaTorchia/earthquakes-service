18515aed6b3f5aec5398f2e00a848f553f6b3b31
SofiaTorchia <sofhia388@gmail.com>
2026-04-30 10:51:50 +0200

    updated files

--- frontend/dashboard.py
+++ frontend/dashboard.py
@@ -10,7 +10,7 @@ import requests
 
 #API_URL = "http://127.0.0.1:8000/earthquakes"
 #API_URL = "https://earthquakes-api-production.up.railway.app/earthquakes"
-API_URL = "https://earthquakes-service.railway.internal:8000/earthquakes"
+API_URL = "https://earthquakes-service.railway.internal:8080/earthquakes"
 
 
 
