4d3ce86aca91272347db1c9a464f658a78852497
SofiaTorchia <sofhia388@gmail.com>
2026-04-30 10:23:13 +0200

    updated api_url to railway internal backend api url

--- frontend/dashboard.py
+++ frontend/dashboard.py
@@ -9,7 +9,7 @@ import pandas as pd
 import requests
 
 #API_URL = "http://127.0.0.1:8000/earthquakes"
-API_URL = "http://earthquakes-service.railway.internal:8000/earthquakes"
+API_URL = "https://earthquakes-api-production.up.railway.app/earthquakes"
 
 
 
