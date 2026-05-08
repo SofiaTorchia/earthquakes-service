7eb6c4a0a109c549265f27065998708cdfb7cac1
SofiaTorchia <sofhia388@gmail.com>
2026-05-08 22:56:18 +0200

    exploring wordpop AUS data

--- population.py
+++ population.py
@@ -1,26 +1,37 @@
 import rasterio
 import numpy as np
 import matplotlib.pyplot as plt
+import time
 
 
 
 if __name__ == "__main__":
     raster_data_path = "aus_ppp_2020.tif"
-
-    with rasterio.open(raster_data_path) as ds:
-        data = ds.read(1,out_shape=(
-                int(ds.height * 0.001),
-                int(ds.width * 0.001)
-            )
-        )
-        print(ds.nodata)
-        nodata_value = ds.nodata
-    masked = np.ma.masked_equal(data, nodata_value)
     
-    plt.figure(figsize=(10, 10))
-    plt.imshow(masked, cmap="viridis", interpolation="nearest")
+    start_time = time.time()
+    with rasterio.open("aus_ppp_2020.tif") as ds:
+        n = 10000
+        resample = False
+        #print(ds.width, ds.height)
+
+        window = rasterio.windows.Window(20000, 20000, n, n)
+        if resample:
+            data = ds.read(1,out_shape=(int(ds.height * 0.01), int(ds.width * 0.01)
+                ), resampling=rasterio.enums.Resampling.average
+            )
+        else:
+            data = ds.read(1, window=window)
+            #data = ds.read(1)
+    
+    nodata_value = ds.nodata
+    masked = np.ma.masked_equal(data, nodata_value)
+    print("--- %s seconds ---" % (time.time() - start_time))
+
+    plt.imshow(masked, cmap="viridis")
     plt.colorbar(label="Population count")
-    plt.title("WorldPop Population Density (masked)")
+    plt.title("WorldPop Population Count (masked)")
     plt.axis("off")
-    plt.savefig("population_density.png", dpi=300, bbox_inches="tight")
+    #plt.savefig("population_count.png", dpi=300, bbox_inches="tight")
     plt.show()
+    
+  
